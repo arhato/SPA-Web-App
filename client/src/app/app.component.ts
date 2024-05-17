44dc5c71a0903d87c11cb8926c9188afbfe7f606
Arrhat Maharjan <arhato23@gmail.com>
2024-05-17 01:16:12 +0100

    Added header with bootsrap.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/partials/header/header.component";
-import { FooterComponent } from "./components/partials/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
